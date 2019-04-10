a0e83b8555a05b0dcb5ae0b20a62494be8d17d46
Kabaye <svkulich@gmail.com>
2019-04-10 18:29:40 +0300

    feat: add customer dao interface and implementation

---
+++ customer-management/src/main/java/com/netcracker/edu/kulich/dao/CustomerDAOImplementation.java
@@ -0,0 +1,32 @@
+package com.netcracker.edu.kulich.dao;
+
+import com.netcracker.edu.kulich.entity.Customer;
+
+import java.util.List;
+
+public class CustomerDAOImplementation implements CustomerDAO {
+    @Override
+    public Customer create(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer read(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
