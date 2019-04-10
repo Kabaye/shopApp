8c5a37f9d421ee018575d8bcde8e76eb37e430d9
Kabaye <svkulich@gmail.com>
2019-04-10 20:33:22 +0300

    feat: add type of strategy to generated value

--- customer-management/src/main/java/com/netcracker/edu/kulich/entity/Customer.java
+++ customer-management/src/main/java/com/netcracker/edu/kulich/entity/Customer.java
@@ -5,22 +5,19 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.StringJoiner;
 
 @Entity
-@Table (name = "customers")
+@Table(name = "customers")
 @NoArgsConstructor
 public class Customer {
 
+    @NonNull
     @Getter
     @Setter
     @Id
-    @GeneratedValue
-    @NonNull
+    @GeneratedValue (strategy = GenerationType.SEQUENCE)
     private long id;
 
     @Getter
