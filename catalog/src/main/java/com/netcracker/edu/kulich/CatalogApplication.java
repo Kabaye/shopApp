c65d46fc6c39212d105cb63e9873b504f821873e
Kabaye <svkulich@gmail.com>
2019-04-10 15:36:10 +0300

    feat: add springboot

--- catalog/src/main/java/com/netcracker/edu/kulich/CatalogApplication.java
+++ catalog/src/main/java/com/netcracker/edu/kulich/CatalogApplication.java
@@ -1,7 +1,11 @@
 package com.netcracker.edu.kulich;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CatalogApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(CatalogApplication.class, args);
     }
 }
