97d5ec460268ce800a352bea227d878f85aec1f2
Kabaye <svkulich@gmail.com>
2019-04-10 17:48:49 +0300

    feat: add DatabaseManager and implementation

---
+++ customer-management/src/main/java/com/netcracker/edu/kulich/utils/DatabaseManager.java
@@ -0,0 +1,7 @@
+package com.netcracker.edu.kulich.utils;
+
+import javax.persistence.EntityManager;
+
+public interface DatabaseManager {
+    EntityManager getEntityManager();
+}
---
+++ customer-management/src/main/java/com/netcracker/edu/kulich/utils/PostgreSQLDatabaseManager.java
@@ -0,0 +1,24 @@
+package com.netcracker.edu.kulich.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PostgreSQLDatabaseManager implements DatabaseManager {
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("userPersistenceUnit");
+    private EntityManager entityManager;
+
+    private PostgreSQLDatabaseManager() {
+        entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
+
+    private final static DatabaseManager INSTANCE = new PostgreSQLDatabaseManager();
+
+    public static DatabaseManager getInstance() {
+        return PostgreSQLDatabaseManager.INSTANCE;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+}
