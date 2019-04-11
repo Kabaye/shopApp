7ad95cfb4328552b10d1825ecc9333beba63af6a
Kabaye <svkulich@gmail.com>
2019-04-11 18:45:42 +0300

    fix: change name

--- catalog/src/main/java/com/netcracker/edu/kulich/dao/TagDAO.java
+++ catalog/src/main/java/com/netcracker/edu/kulich/dao/TagDAO.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public interface TagDAO {
     Tag create(Tag tag);
     Tag read(Long id);
-    Set<Tag> create(Set<Tag> categories);
+    Set<Tag> create(Set<Tag> tags);
     Tag update(Tag tag);
     void delete(Long id);
 }
