fe128a1eb2e21124110d253e96e4f2aa47e494e6
Kabaye <svkulich@gmail.com>
2019-04-11 18:07:50 +0300

    feat: add OfferDAO implementation

---
+++ catalog/src/main/java/com/netcracker/edu/kulich/dao/OfferDAOImplementation.java
@@ -0,0 +1,59 @@
+package com.netcracker.edu.kulich.dao;
+
+import com.netcracker.edu.kulich.entity.Offer;
+import com.netcracker.edu.kulich.utils.PostgreSQLDatabaseManager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.List;
+
+public class OfferDAOImplementation implements OfferDAO {
+
+    private EntityManager entityManager = PostgreSQLDatabaseManager.getInstance().getEntityManager();
+
+    @Override
+    public Offer create(Offer offer) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(offer);
+        transaction.commit();
+        return offer;
+    }
+
+    @Override
+    public Offer read(Long id) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Offer foundOffer = entityManager.find(Offer.class, id);
+        transaction.commit();
+        return foundOffer;
+    }
+
+    @Override
+    public List<Offer> findAll() {
+        EntityTransaction transaction = entityManager.getTransaction();
+        List<Offer> offers;
+        transaction.begin();
+        offers = entityManager.createQuery("select offer from Offer offer", Offer.class).getResultList();
+        transaction.commit();
+        return offers;
+    }
+
+    @Override
+    public Offer update(Offer offer) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.merge(offer);
+        transaction.commit();
+        return offer;
+    }
+
+    @Override
+    public void delete(Long id) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Offer offer = entityManager.getReference(Offer.class, id);
+        entityManager.remove(offer);
+        transaction.commit();
+    }
+}
