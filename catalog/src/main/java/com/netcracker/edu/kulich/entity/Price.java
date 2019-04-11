7fa5704a6afd1ff548163ba47a9e23405707f3f7
Kabaye <svkulich@gmail.com>
2019-04-11 16:43:07 +0300

    feat: add toString() method

--- catalog/src/main/java/com/netcracker/edu/kulich/entity/Price.java
+++ catalog/src/main/java/com/netcracker/edu/kulich/entity/Price.java
@@ -6,6 +6,7 @@ import lombok.NonNull;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.StringJoiner;
 
 @Entity
 @Table (name = "prices")
@@ -17,10 +18,19 @@ public class Price {
     @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "price_id")
     private long id;
 
     @Getter
     @Setter
     @NonNull
     private double price;
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Price.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("price=" + price)
+                .toString();
+    }
 }
