1546222cb735e940e9d8decd4af37ee6d09f7f8e
Kabaye <svkulich@gmail.com>
2019-04-16 13:19:49 +0300

    feat: add status enums

---
+++ inventory/src/main/java/com/netcracker/edu/kulich/entity/OrderPaymentStatusEnum.java
@@ -0,0 +1,9 @@
+package com.netcracker.edu.kulich.entity;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public enum OrderPaymentStatusEnum {
+    NOT_PAID,
+    PAID
+}
---
+++ inventory/src/main/java/com/netcracker/edu/kulich/entity/OrderStatusEnum.java
@@ -0,0 +1,11 @@
+package com.netcracker.edu.kulich.entity;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public enum OrderStatusEnum {
+    IN_PROCESS, //в процессе сборки
+    AGGREGATED, //собран
+    SHIPPED, //отправлен
+    DELIVERED // доставлен
+}
