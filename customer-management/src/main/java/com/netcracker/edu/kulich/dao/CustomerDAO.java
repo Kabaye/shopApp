0ddcb34d58d4acfc56339abbdbbb1e1c3a4d4ae4
Kabaye <svkulich@gmail.com>
2019-04-29 19:48:30 +0300

    style:

--- customer-management/src/main/java/com/netcracker/edu/kulich/dao/CustomerDAO.java
+++ customer-management/src/main/java/com/netcracker/edu/kulich/dao/CustomerDAO.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface CustomerDAO {
     Customer save(Customer customer);
+
     Customer getById(Long id);
+
     List<Customer> findAll();
+
     Customer update(Customer customer);
+
     void deleteById(Long id) throws EntityNotFoundException;
 }
