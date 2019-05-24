c9fe5fd924dd045edba8dc2b2f648c47293e6a5c
Kabaye <svkulich@gmail.com>
2019-05-24 22:15:27 +0300

    feat: add eureka

--- catalog/src/main/java/com/netcracker/edu/kulich/CatalogApplication.java
+++ catalog/src/main/java/com/netcracker/edu/kulich/CatalogApplication.java
@@ -2,7 +2,9 @@ package com.netcracker.edu.kulich;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CatalogApplication {
     public static void main(String[] args) {
